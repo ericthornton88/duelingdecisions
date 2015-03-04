4374195d7596a85ecd10bb60a912a3f502ec62a9
Eric Thornton <thornton.w.eric@gmail.com>
2015-03-03 21:42:38 -0700

    css update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,8 @@
 @extends('layout')
 
 @section('main_content')
-<<<<<<< HEAD
 	<h1>Your Decision is in the Ring!</h1>
-=======
-	<h1>Your Decisions in the Ring!</h1>
-	<span class="cloud"><i class="fa fa-cloud fa-5x"></i></span>
->>>>>>> 7717d0bf283472f52bb5b0b4979a9a3935760335
+	{{-- <span class="cloud"><i class="fa fa-cloud fa-5x"></i></span> --}}
 	<main>
 		<div class="brief-info">
 			<h3>Having doubts about registering?</h3>
@@ -16,7 +12,7 @@
 			 similique obcaecati ipsa illum, maxime quia quas nisi officiis unde corporis voluptatem temporibus dolor sunt.</div>
 			<a href="#">More...</a>
 		</div>
-		<div class="logo"><a href="/"><img src="/img/logo.png" alt="Home"></a> </div>
+		{{-- <div class="logo"><a href="/"><img src="/img/logo.png" alt="Home"></a> </div> --}}
 		<div class="credentials">
 			<h3>Login here:</h3>
 			<form action="">
