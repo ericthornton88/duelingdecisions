d612a4c9b12c87ab981758e9351f5ec3f28a6d6c
kdragos <kristindragos@gmail.com>
2015-03-04 10:07:46 -0700

    added basic duel page html

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,6 +5,8 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
+Route::get('duel', addcontrollerhere!!);
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
