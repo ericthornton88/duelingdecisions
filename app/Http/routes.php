7b549ff1c20b19d0467ecc4f805bf01a95d42888
Eric Thornton <thornton.w.eric@gmail.com>
2015-03-04 09:58:30 -0700

    registration page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,6 +5,8 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
+Route::get('registration', 'RegistrationController@index');
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
