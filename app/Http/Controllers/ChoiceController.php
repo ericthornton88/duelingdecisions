cabd2435b91107ff73c5449388c7c68e11e326af
kdragos <kristindragos@gmail.com>
2015-03-04 16:29:57 -0700

    choice model

---
+++ app/Http/Controllers/ChoiceController.php
@@ -0,0 +1,21 @@
+<?php namespace App\Http\Controllers;
+
+use App\Library\ChoiceModel;
+use DB;
+
+class ChoiceController extends Controller {
+
+
+	public function getChoices() {
+		$all = new ChoiceModel();
+		$all_categories = $all->getCategory();
+		print_r($all_categories);
+
+		return view('/category/new', ['category'=>$all_categories]);
+	}
+
+	public function newChoice() {
+
+	}
+
+}
---
+++ app/Library/ChoiceModel.php
@@ -0,0 +1,49 @@
+<?php 
+namespace App\Library;
+use DB;
+//class here. 
+//name php file exactly what you make the class name.
+//a new file for a new class
+class ChoiceModel {
+	
+	public function getChoices() {
+		$results = DB::select('
+		SELECT * 
+		FROM choice
+		');
+		
+		return $results;
+	}
+
+		
+	public function newChoice($user_id, $category_id) {
+	
+		DB::insert(
+			'INSERT INTO choice 
+			(category_id, user_id, details, title) 
+			values (:category_id, :user_id, :details, :title)',
+			[':user_id' 	=> $user_id,
+			 ':category_id' => $category_id,
+			 ':details' 	=> $details,
+			 ':title' 		=> $title,
+			]);
+		$insert_id = DB::getPdo()->lastInsertId();
+		return userChoiceFeature($insert_id);
+	};	
+	
+
+	public function userChoiceFeature($insert_id){
+		DB::insert(
+			'INSERT INTO user_choice_feature 
+			(choice_id, is_positive, rating, description) 
+			values (:choice_id, :is_positive, :rating, :description)',
+			[':is_positive' 	=> $is_positive,
+			 ':choice_id' 		=> $insert_id,
+			 ':rating' 			=> $rating,
+			 ':description' 	=> $description,
+			]);
+		return redirect('/');
+	}	
+	
+
+}
