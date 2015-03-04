944e88c363dbabb81e12d9a86d534a21593fe19f
Eric Thornton <thornton.w.eric@gmail.com>
2015-03-04 10:07:16 -0700

    html of registration

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -2,6 +2,21 @@
 
 @section('main_content')
 <div>
-	This is the registration page
+	Register to duel!
+</div>
+<div>
+	<form action="">
+		Full Name: <input type="text">
+		Email: <input type="text">
+		Password: <input type="password">
+		Re-enter password: <input type="password">
+		<select name="gender">
+			<option value="m">Male</option>
+			<option value="f">Female</option>
+			<option value="u">Choose not to Answer</option>
+		</select>
+		Gender: <input type="text">
+		<button>Submit</button>
+	</form>
 </div>
 @endsection
