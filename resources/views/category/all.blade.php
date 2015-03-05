96ffc8c45e285553c965d136f0eb3a821e33bdbb
Garrett Tacoronte <garretttacoronte@gmail.com>
2015-03-04 17:48:00 -0700

    BOOM

--- resources/views/category/all.blade.php
+++ resources/views/category/all.blade.php
@@ -1,25 +1,25 @@
 @extends('layout')
 
 @section('main_content')
-	
 	<h1>All Categories and Choices</h1>
 
-	<?php $cur_cat = ''; ?>
-	<div class="cat-container">
-	<ul class="category">
-	@foreach($category as $cat)
-		@if($cur_cat == $cat->category_name)
-			<div class="options">{{$cat->title}}</div>
-		@else 
-			</ul>
-			<?php $cur_cat = $cat->category_name;?>
-			
-			<ul class="category"> <li>{{$cat->category_name}} <div class="options">{{$cat->title}}</div></li>
-		@endif
-	@endforeach
-	</ul>
+	<div class="main-cat-container">
+		<?php $cur_cat = ''; ?>
+		<div class="cat-container">
+		<ul class="category">
+		@foreach($category as $cat)
+			@if($cur_cat == $cat->category_name)
+				<div class="options">{{$cat->title}}</div>
+			@else 
+				</ul>
+				<?php $cur_cat = $cat->category_name;?>
+				
+				<ul class="category"> <li>{{$cat->category_name}} <div class="options">{{$cat->title}}</div></li>
+			@endif
+		@endforeach
+		</ul>
+		</div>
 	</div>
-
 		
 @endsection
 
