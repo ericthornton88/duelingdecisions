c5e6a30d7c7233e94d262faadd36e34dc2186fb2
Eric Thornton <thornton.w.eric@gmail.com>
2015-03-10 11:02:22 -0700

    button toggle

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -15,28 +15,44 @@
 <body>
 	
 	@section('header')
+	<?php
+	use App\User;
+	if (Auth::guest()) {
+		$welcome = 'Login';
+		$option_1 = 'Login';
+		$link_1 = '/';
+		$logo_1 = 'fa fa-sign-in';
+		$option_2 = 'Register';
+		$link_2 = 'auth/register';
+		$logo_2 = 'fa fa-plus-square';
+	} else {
+		$user = \Auth::user()->first_name;
+		$welcome = 'Welcome ' . $user;
+		$option_1 = 'New Duel';
+		$link_1 = 'profile';
+		$logo_1 = 'fa fa-star';
+		$option_2 = 'Logout';
+		$link_2 = 'auth/logout';
+		$logo_2 = 'fa fa-sign-out';
+		// $option_3 = 'New';
+	}
+	
+	
+	?>
 	<header>
 		<div class="btn-group open">
-		  <a class="btn btn-primary" href="#"><i class="fa fa-user fa-fw"></i>Welcome User</a>
-		  {{-- <a class="btn btn-primary dropdown-toggle" data-toggle="dropdown" href="#">
-		    <span class="fa fa-caret-down"></span></a> --}}
+		  <a class="btn btn-primary" href="#"><i class="fa fa-user fa-fw"></i>{{$welcome}}</a>
 		  <ul class="dropdown-menu">
-		    <li><a href="#"><i class="fa fa-pencil fa-fw"></i> Edit</a></li>
-		    <li><a href="#"><i class="fa fa-trash-o fa-fw"></i> Delete</a></li>
-		    <li><a href="#"><i class="fa fa-ban fa-fw"></i> Ban</a></li>
+		    <li><a href="{{$link_1}}"><i class="{{$logo_1}} fa-fw"></i>{{$option_1}}</a></li>
+		    <li><a href="{{$link_2}}"><i class="{{$logo_2}} fa-fw"></i>{{$option_2}}</a></li>
+		    {{-- <li><a href="#"><i class="fa fa-ban fa-fw"></i>{{$option_3}}</a></li> --}}
 		  </ul>
 		</div>
 		<div class="hero">
 			<a href="/"><img src="/img/logo2.png" alt="Dueling Decisions"></a>
-			{{-- <nav>
-				<a href="/">Home</a> |
-				<a href="/profile"> New Duel</a> |
-				<a href="/"> Sign In</a> |
-				<a href="logout"> Logout</a>
-			</nav> --}}
 			<h1>
-			@section('title')
-			@show
+				@section('title')
+				@show
 			</h1>
 		</div>	
 	</header>
