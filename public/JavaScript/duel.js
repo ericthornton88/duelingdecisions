e219821c312fbe8065035d8baede0ca96148d3a4
Garrett Tacoronte <garretttacoronte@gmail.com>
2015-03-11 09:47:32 -0700

    yay

--- public/JavaScript/duel.js
+++ public/JavaScript/duel.js
@@ -1,8 +1,8 @@
 var duelInit = function(){
 	$('.choice-one').text(c[0].title);
-	$('.pro').text(c[0].description);
+	$('.pro').text(c[0].details);
 	$('.choice-two').text(c[1].title);
-	$('.con').text(c[1].description);
+	$('.pro2').text(c[1].details);
 };
 
 $(function(){
